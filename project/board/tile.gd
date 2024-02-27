43e335384f41a9dda971db87242e0ec090b57bfd
Jadi Miller <jadilmiller@gmail.com>
2024-02-27 15:00:43 -0500

    Register when Tile is clicked on

--- project/board/tile.gd
+++ project/board/tile.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+var selected := false
+var mouse_entered := false
 
-func _ready():
-	pass
+
+func _input(event):
+	if mouse_entered and event.is_action_released("click"):
+		selected = true
+		print(selected)
+
+
+func _on_mouse_catcher_mouse_entered():
+	mouse_entered = true
+
+
+func _on_mouse_catcher_mouse_exited():
+	mouse_entered = false
