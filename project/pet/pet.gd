9304e1b39c5334736b903c9ab2a264864e140f2e
Brooke Wright <brookerwright2@gmail.com>
2024-03-21 11:46:24 -0400

    Add a third growth stage for Pet

--- project/pet/pet.gd
+++ project/pet/pet.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 enum Age {
 	BABY,
-	ADULT
+	TEEN,
+	ADULT,
 }
 
-var _baby_image := preload("res://pet/hedgehog_baby_test.png")
+var _baby_image := preload("res://pet/hedgehog_baby.png")
+var _teen_image := preload("res://icon.svg")
 var _adult_image := preload("res://pet/hedgehog_adult_standing.png")
 
 var _touching_player := false
@@ -14,8 +16,12 @@ var _growth_state: int:
 	set(state):
 		if state == Age.BABY:
 			pet_sprite.texture = _baby_image
+		elif state == Age.TEEN:
+			pet_sprite.texture = _teen_image
 		else:
+			print("hello?")
 			pet_sprite.texture = _adult_image
+		_growth_state = state
 
 @onready var pet_sprite : Sprite2D = $Sprite2D
 
@@ -27,8 +33,14 @@ func set_popup_menu_position(pos : Vector2) -> void:
 	$PopupMenu.position = pos
 
 func _grow_up() -> void:
+	print(_growth_state)
 	if _growth_state == Age.BABY:
+		print("ok")
+		_growth_state = Age.TEEN
+	elif _growth_state == Age.TEEN:
+		print('teen')
 		_growth_state = Age.ADULT
+	print(_growth_state)
 
 
 func _on_board_confirmed_pet_selected() -> void:
