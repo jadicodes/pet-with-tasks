88fd6e06c251bd141235bcfbdf1fc897163bd905
Jadi Miller <jadilmiller@gmail.com>
2024-03-26 11:52:54 -0400

    Change Crop sprite depending on Crop growth state

--- project/object/crop/crop.gd
+++ project/object/crop/crop.gd
@@ -1,8 +1,29 @@
 extends Node2D
 
+enum Age {
+	NOT_READY,
+	READY,
+}
 
+var _ready_image := preload("res://object/crop/crop_placeholder_2.png")
+var _not_ready_image := preload("res://icon.svg")
+
+@onready var crop_sprite : Sprite2D = $Sprite2D
 @onready var _player_detector : PlayerDetector = $PlayerDetector
 @onready var _popup : PopupMenu = $PopupMenu
+
+# In order for set() to go off, cannot be initially declared
+var _growth_state: int: 
+	set(state):
+		if state == Age.NOT_READY:
+			crop_sprite.texture = _not_ready_image
+		elif state == Age.READY:
+			crop_sprite.texture = _ready_image
+		_growth_state = state
+
+
+func _ready() -> void:
+	_growth_state = Age.NOT_READY
 
 
 func handle_selected() -> void:
@@ -10,8 +31,17 @@ func handle_selected() -> void:
 		$PopupMenu.show()
 
 
-func _on_popup_menu_id_pressed(_id):
-	print("Yippeeeeeeeeeeee") # it will be yipped
+func _on_popup_menu_id_pressed(id):
+	if id == 0:
+		_grow_up()
+
 
 func set_popup_menu_position(pos : Vector2) -> void:
 	_popup.position = pos
+
+
+func _grow_up() -> void:
+	if _growth_state == Age.NOT_READY:
+		_growth_state = Age.READY
+	elif _growth_state == Age.READY:
+		_growth_state = Age.NOT_READY
